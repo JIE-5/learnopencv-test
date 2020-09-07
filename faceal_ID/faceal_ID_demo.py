e6fdcaf5df3a50d0046ea47229fd7c6a7589f82c
JIE <70597217+JIE-5@users.noreply.github.com>
2020-09-07 09:05:39 +0800

    i dont know what happen

--- faceal_ID/faceal_ID_demo.py
+++ faceal_ID/faceal_ID_demo.py
@@ -5,19 +5,19 @@ import numpy as np
 def faceal_ID_demo():
     face_casecade=cv2.CascadeClassifier('/home/jie-5/learnopencv-test/faceal_ID/haarcascade_frontalface_alt.xml')
     camera = cv2.VideoCapture(0)
-    camera.set(15, 300)
+    # camera.set(15, 300)
     cv2.namedWindow('face')
 
     while True:
         # print("exposure={}".format(camera.get(15)))
 
-        print(camera.get(15))
+        # print(camera.get(15))
         ret, frame = camera.read()
 
-        if not ret :
+        if not ret:
             break
         gray_farme = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        face = face_casecade.detectMultiScale(gray_farme, 1.5, 2)
+        face = face_casecade.detectMultiScale(gray_farme, 1.3, 4)
         # print(face)
         for x, y, w, h in face:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
